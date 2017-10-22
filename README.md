5fb4ff73060f92a9fdc8e4b96126f7d4dc6e88cb
painkuter <kuter91@gmail.com>
2017-10-22 23:15:02 +0300

    matrix

---
+++ README.md
@@ -0,0 +1,1 @@
+go get github.com/mitsuse/matrix-go
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//dense"github.com/mitsuse/matrix-go"
+	"github.com/mitsuse/matrix-go/dense"
+)
+
+const (
+	learningRate = 0.1
+)
 
 func main() {
 	fmt.Println("Test")
+	m := dense.New(2, 3)(
+		0, 1, 2,
+		3, 4, 5,
+	)
+	fmt.Println(m)
+}
+
+type Network struct {
+	FirstLvlWeight  [][]float64
+	SecondLvlWeight []float64
 }
