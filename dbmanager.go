65e615040135b5fa154de46110c2aa0b43a3c8f5
painkuter <kuter91@gmail.com>
2017-11-20 23:57:52 +0300

    working version

---
+++ dbmanager.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const dbNname = "trading"
+
+var connections = []string{
+	"test",
+	"root:111@tcp(127.0.0.1:3306)/",
+	"root:12345678@tcp(127.0.0.1:3306)/",
+	"root@tcp(127.0.0.1:3306)/",
+	"root:111@localhost/",
+}
+
+func getMySQL() string {
+	b, err := ioutil.ReadFile("dev/mtest_users.sql") // just pass the file name
+	if err != nil {
+		fmt.Println("ERROR Getting SQL file: " + err.Error())
+	}
+	return string(b) // convert content to a 'string'
+}
+
+func DB() *sql.DB {
+	var (
+		//dbConnect string
+		db  *sql.DB
+		err error
+	)
+
+	for _, el := range connections {
+		db, err = sql.Open("mysql", el)
+		result, _ := db.Exec("CREATE DATABASE IF NOT EXISTS " + dbNname)
+		db, err = sql.Open("mysql", el+dbNname)
+		if result != nil {
+			fmt.Println("CONNECTED TO DB-SERVER: " + el)
+			break
+		}
+	}
+	if err != nil {
+		fmt.Println("DB Initialization finished with errors:")
+		panic(err)
+	} else {
+		fmt.Printf("DB Initialization finished successfully [%v]\n", dbNname)
+	}
+	// defer db.Close()
+	return db
+}
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	epochs       = 100000
+	epochs       = 1
 	learningRate = 0.01
 	m            = 3 // first layer
 	n            = 3 // hidden layer
@@ -39,9 +39,12 @@ var train = []Input{
 var train2 = append(train, Input{[]float64{1, 1, 1}, 1})
 
 func main() {
+	db := DB()
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS attempts (`id` INT(10) NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`))")
+	if err != nil {
+		panic(err)
+	}
 	net := Init(n, m)
-	//Print(net.FirstLvlWeight)
-	//Print(net.HiddenLvlWeight)
 	t := time.Now().UnixNano()
 
 	for i := 0; i < epochs; i++ {
@@ -57,7 +60,7 @@ func main() {
 		}
 
 		err := MSE(actual_results, correct_results)
-		fmt.Printf("\rProcess: %v ERROR: %1.6v", 100*i/epochs, err)
+		fmt.Printf("\rProcess: %v%% ERROR: %1.6v", 100*i/epochs, err)
 		if err < 0.001 {
 			break
 		}
@@ -76,108 +79,82 @@ func main() {
 }
 
 func (this Network) Train(train Input) {
-	/*//predict
-		in1 := mat.NewDense(m, 1, train.Row) //[m*1]
-		out1 := mat.NewDense(n, 1, nil)      //[n*1]
-		out1.Mul(this.FirstLvlWeight, in1)   //[n*m]*[m*1] = [n*1]?
-		in2 := SigmoidMap(out1)              //[n*1]
-
-		out2 := mat.NewDense(1, 1, nil)
-		out2.Mul(this.HiddenLvlWeight, in2)
-		result := SigmoidMap(out2)
-		actual := result.At(0, 0) // = out2
-
-		fmt.Printf("\rERROR: %v", MSE(actual, train.ExpectedPredict))
-		//get error
-		err2 := actual - train.ExpectedPredict
-		gradient2 := actual * (1 - actual) // sigmoid(x)dx|x=actual
-		dWeight2 := err2 * gradient2
-
-		// w2 = w2 - in2 * learningRate * d_w : [1*n] = [1*n] - [n*1]^T * [1] * [1]
-		tmpWeights2 := mat.DenseCopyOf(in2.T()) //
-		tmpWeights2.Scale((-1)*dWeight2*learningRate, tmpWeights2)
-		this.HiddenLvlWeight.Add(this.HiddenLvlWeight, tmpWeights2) //updated weights
-
-		//обратное распространение
-
-		err1 := mat.NewDense(1, n, nil)            // [1*n]
-		err1.Scale(dWeight2, this.HiddenLvlWeight) //[1*n]
-		gradient1 := SigmoidMapDx(in2).T()             //[n*1]
-		//Print(gradient1)
-		dWeight1 := mat.NewDense(1, n, nil)   //[1*n]
-		dWeight1.MulElem(err1, gradient1) //[1*n] ** [1*n] Hadamard product
-
-		tmpWeights1 := mat.NewDense(m, n, nil) // [m*n]
-		tmpWeights1.Mul(in1, dWeight1) // [m*1] * [1*n]
-		tmpWeights1.Scale((-1)*learningRate, tmpWeights1)
-	//Print(this.FirstLvlWeight)
-		this.FirstLvlWeight.Add(this.FirstLvlWeight, tmpWeights1.T())
-		return*/
+	////predict
+	//	in1 := mat.NewDense(m, 1, train.Row) //[m*1]
+	//	out1 := mat.NewDense(n, 1, nil)      //[n*1]
+	//	out1.Mul(this.FirstLvlWeight, in1)   //[n*m]*[m*1] = [n*1]?
+	//	in2 := SigmoidMap(out1)              //[n*1]
+	//
+	//	out2 := mat.NewDense(1, 1, nil)
+	//	out2.Mul(this.HiddenLvlWeight, in2)
+	//	result := SigmoidMap(out2)
+	//	actual := result.At(0, 0) // = out2
+	//
+	//	//get error
+	//	err2 := actual - train.ExpectedPredict
+	//	gradient2 := actual * (1 - actual) // sigmoid(x)dx|x=actual
+	//	dWeight2 := err2 * gradient2
+	//
+	//	// w2 = w2 - in2 * learningRate * d_w : [1*n] = [1*n] - [n*1]^T * [1] * [1]
+	//	tmpWeights2 := mat.DenseCopyOf(in2.T()) //
+	//	tmpWeights2.Scale((-1)*dWeight2*learningRate, tmpWeights2)
+	//	this.HiddenLvlWeight.Add(this.HiddenLvlWeight, tmpWeights2) //updated weights
+	//
+	//	//обратное распространение
+	//
+	//	err1 := mat.NewDense(1, n, nil)            // [1*n]
+	//	err1.Scale(dWeight2, this.HiddenLvlWeight) //[1*n]
+	//	gradient1 := SigmoidMapDx(in2).T()             //[n*1]
+	//	//Print(gradient1)
+	//	dWeight1 := mat.NewDense(1, n, nil)   //[1*n]
+	//	dWeight1.MulElem(err1, gradient1) //[1*n] ** [1*n] Hadamard product
+	//
+	//	tmpWeights1 := mat.NewDense(m, n, nil) // [m*n]
+	//	tmpWeights1.Mul(in1, dWeight1) // [m*1] * [1*n]
+	//	tmpWeights1.Scale((-1)*learningRate, tmpWeights1)
+	//	this.FirstLvlWeight.Add(this.FirstLvlWeight, tmpWeights1.T())
+	//	return
 
 	//inputs_1 = np.dot(self.weights_0_1, inputs)
-	inputs := mat.NewDense(m, 1, train.Row) // [m*1]
-	inputs_1 := mat.NewDense(n, 1, nil)
-	inputs_1.Mul(this.FirstLvlWeight, inputs)
-	//outputs_1 = self.sigmoid_mapper(inputs_1)
-	outputs_1 := SigmoidMap(inputs_1)
-	//inputs_2 = np.dot(self.weights_1_2, outputs_1)
-	inputs_2 := mat.NewDense(1, 1, nil)
-	inputs_2.Mul(this.HiddenLvlWeight, outputs_1)
-	//outputs_2 = self.sigmoid_mapper(inputs_2)
-	outputs_2 := SigmoidMap(inputs_2)
-	//actual_predict = outputs_2[0]
-	actual_predict := outputs_2.At(0, 0)
-	//
-	//error_layer_2 = np.array([actual_predict - expected_predict])
-	error_layer_2 := actual_predict - train.ExpectedPredict
-	//gradient_layer_2 = actual_predict * (1 - actual_predict)
-	gradient_layer_2 := actual_predict * (1 - actual_predict)
+	in := mat.NewDense(m, 1, train.Row) // [m*1]
+	in1 := mat.NewDense(n, 1, nil)
+	in1.Mul(this.FirstLvlWeight, in)
+	out1 := SigmoidMap(in1)
+	in2 := mat.NewDense(1, 1, nil)
+	in2.Mul(this.HiddenLvlWeight, out1)
+	out2 := SigmoidMap(in2)
+	actual := out2.At(0, 0)
+
+	//BackPrepagation
+	//hidden layer
+	err2 := actual - train.ExpectedPredict
+	grad2 := actual * (1 - actual)
 	//gradient_layer_2 := SigmoidDx(actual_predict)
-	//weights_delta_layer_2 = error_layer_2 * gradient_layer_2
-	weights_delta_layer_2 := error_layer_2 * gradient_layer_2
-	//self.weights_1_2 -= (np.dot(weights_delta_layer_2, outputs_1.reshape(1, len(outputs_1)))) * self.learning_rate
-	tmpWeights2 := mat.DenseCopyOf(outputs_1.T()) //
-	tmpWeights2.Scale((-1)*weights_delta_layer_2*learningRate, tmpWeights2)
+	dWeight2 := err2 * grad2
+	tmpWeights2 := mat.DenseCopyOf(out1.T()) //
+	tmpWeights2.Scale((-1)*dWeight2*learningRate, tmpWeights2)
 	this.HiddenLvlWeight.Add(this.HiddenLvlWeight, tmpWeights2) //updated weights
-	//
-	//error_layer_1 = weights_delta_layer_2 * self.weights_1_2
-	error_layer_1 := mat.NewDense(1, n, nil)
-	error_layer_1.Scale(weights_delta_layer_2, this.HiddenLvlWeight)
-	//gradient_layer_1 = outputs_1 * (1 - outputs_1)
+
+	//first layer
+	err1 := mat.NewDense(1, n, nil)
+	err1.Scale(dWeight2, this.HiddenLvlWeight)
 	//gradient_layer_1 := SigmoidMapDx(outputs_1)
-	gradient_layer_1 := SigmoidMapDx(outputs_1)
-	//weights_delta_layer_1 = error_layer_1 * gradient_layer_1
-	weights_delta_layer_1 := mat.NewDense(1, n, nil)
-	weights_delta_layer_1.MulElem(error_layer_1, gradient_layer_1.T())
-	//self.weights_0_1 -= np.dot(inputs.reshape(len(inputs), 1), weights_delta_layer_1).T  * self.learning_rate
-	tmpWeights1 := mat.NewDense(m, n, nil)         // [m*n]
-	tmpWeights1.Mul(inputs, weights_delta_layer_1) // [m*1] * [1*n]
+	grad1 := SigmoidMapDx(out1)
+	dWeight1 := mat.NewDense(1, n, nil)
+	dWeight1.MulElem(err1, grad1.T())
+	tmpWeights1 := mat.NewDense(m, n, nil) // [m*n]
+	tmpWeights1.Mul(in, dWeight1)          // [m*1] * [1*n]
 	tmpWeights1.Scale((-1)*learningRate, tmpWeights1)
-	//fmt.Println(tmpWeights1.At(0,0))
-	//Print(this.FirstLvlWeight)
 	this.FirstLvlWeight.Add(this.FirstLvlWeight, tmpWeights1.T())
 }
 
-func (this Network) Predict(in1 *mat.Dense) float64 {
-	/*out1 := mat.NewDense(this.FirstLvlWeight.RawMatrix().Rows, 1, nil)
-	out1.Mul(this.FirstLvlWeight, in1)
-	in2 := SigmoidMap(out1)
-
-	out2 := mat.NewDense(1, 1, nil)
-	out2.Mul(this.HiddenLvlWeight, in2)
-	actual := SigmoidMap(out2)
-
-	return actual*/
-
-	inputs_1 := mat.NewDense(n, 1, nil)
-	inputs_1.Mul(this.FirstLvlWeight, in1)
-	//outputs_1 = self.sigmoid_mapper(inputs_1)
-	outputs_1 := SigmoidMap(inputs_1)
-	//inputs_2 = np.dot(self.weights_1_2, outputs_1)
-	inputs_2 := mat.NewDense(1, 1, nil)
-	inputs_2.Mul(this.HiddenLvlWeight, outputs_1)
-	//outputs_2 = self.sigmoid_mapper(inputs_2)
-	return SigmoidMap(inputs_2).At(0, 0)
+func (this Network) Predict(in *mat.Dense) float64 {
+	in1 := mat.NewDense(n, 1, nil)
+	in1.Mul(this.FirstLvlWeight, in)
+	out1 := SigmoidMap(in1)
+	in2 := mat.NewDense(1, 1, nil)
+	in2.Mul(this.HiddenLvlWeight, out1)
+	return SigmoidMap(in2).At(0, 0)
 }
 
 func Init(n, m int) Network {
